1c449b92ab2ff1c38342873c810d94d09a258ead
KeronGoodridgeMac <KeronGoodridgeMac@kerons-mbp.broadband>
2020-04-13 16:11:49 +0100

    Successfully added validation for the first name and last name fields. A bug with the age validation is alos noticed as it does not accept users born in 1996.

--- register.php
+++ register.php
@@ -17,23 +17,34 @@ if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' )
     $errors = array();
     
     
-    # Check for a title
-    { $pti = mysqli_real_escape_string( $dbc, trim( $_POST[ 'p_title' ] ) ) ; }
+    #PERFORM VALIDATION ON FIELDS
+        # Check for a title
+        { $pti = mysqli_real_escape_string( $dbc, trim( $_POST[ 'p_title' ] ) ) ; }
+        
+        #NAME VALIDATION
+        # Check for a first name.
+        if ( empty( $_POST[ 'first_name' ] ) )
+        { $errors[] = 'Enter your first name.' ; }
+        
+        $fname = $_POST[ 'first_name' ];
+        if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
+            $error[] = "The first name field is not valid: only letter and spaces are allowed";
+        }
+        else
+        { $fn = mysqli_real_escape_string( $dbc, trim( $_POST[ 'first_name' ] ) ) ; }
+        
+        # Check for a last name.
+        if (empty( $_POST[ 'last_name' ] ) )
+        { $errors[] = 'Enter your last name.' ; }
+        
+        $lname = $_POST[ 'last_name' ];
+        if (!preg_match("/^[a-zA-Z ]*$/",$lname)) {
+            $error[] = "The last name field is not valid: only letters and spaces are allowed";
+        }
+        else
+        { $ln = mysqli_real_escape_string( $dbc, trim( $_POST[ 'last_name' ] ) ) ; }
     
-    
-    # Check for a first name.
-    if ( empty( $_POST[ 'first_name' ] ) )
-    { $errors[] = 'Enter your first name.' ; }
-    else
-    { $fn = mysqli_real_escape_string( $dbc, trim( $_POST[ 'first_name' ] ) ) ; }
-    
-    # Check for a last name.
-    if (empty( $_POST[ 'last_name' ] ) )
-    { $errors[] = 'Enter your last name.' ; }
-    else
-    { $ln = mysqli_real_escape_string( $dbc, trim( $_POST[ 'last_name' ] ) ) ; }
-    
-    # DATE OF BIRTH VALIDATION.
+        # DATE OF BIRTH VALIDATION.
         #If not empty.
         if (empty( $_POST[ 'dob' ] ) )
         { $errors[] = 'Enter your DOB.' ; }
@@ -49,12 +60,18 @@ if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' )
         { $p = mysqli_real_escape_string( $dbc, trim( $_POST[ 'pass1' ] ) ) ; }
     
    
-    
-    # Check for an email address:
-    if ( empty( $_POST[ 'email' ] ) )
-    { $errors[] = 'Enter your email address.'; }
-    else
-    { $e = mysqli_real_escape_string( $dbc, trim( $_POST[ 'email' ] ) ) ; }
+        #EMAIL VALIDATION
+        # Check for an email address:
+        if ( empty( $_POST[ 'email' ] ) )
+        { $errors[] = 'Enter your email address.'; }
+        
+        # Check if email address is valid i.e. contains no spaces and @ sign.
+        $email = $_POST[ 'email' ];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "Invalid email format: try including an '@' sign"; 
+        }
+        else
+        { $e = mysqli_real_escape_string( $dbc, trim( $_POST[ 'email' ] ) ) ; }
     
     
     
