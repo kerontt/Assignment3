3ab625e4251db3363900e132d8c2f3ff6035d91f
KeronGoodridgeMac <KeronGoodridgeMac@kerons-mbp.broadband>
2020-04-18 13:37:55 +0100

    Successful testing of the error messages below the submit button. Submit button successfully logs user into system.

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<html>
+
 
 
 
@@ -16,6 +16,7 @@ $page_title = 'Login' ;
 include ( 'includes/header.html' ) ;
 include ( 'includes/menu.html' ) ;
 
+
 # Display any error messages if present.
 if ( isset( $errors ) && !empty( $errors ) )
 {
@@ -30,7 +31,7 @@ if ( isset( $errors ) && !empty( $errors ) )
     <div class="col-md-8 col-lg-6">
       <div class="login d-flex align-items-center py-5">
         <div class="container">
-          <div class="row">
+          
             <div class="col-md-9 col-lg-8 mx-auto">
             
 
@@ -41,6 +42,7 @@ if ( isset( $errors ) && !empty( $errors ) )
  <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required autofocus>
 
 </div>
+<p></p>
 
 <div class="form-label-group">
                   <input type="password" id="inputPassword" name="pass" class="form-control" placeholder="Password" required>
@@ -55,6 +57,15 @@ if ( isset( $errors ) && !empty( $errors ) )
 <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit" value="Login">Sign in</button>
 
 </form>
+<?php 
+
+# Display any error messages if present.
+if ( isset( $errors ) && !empty( $errors ) )
+{
+    echo '<p id="err_msg">Oops! Something went wrong!:<br>' ;
+    foreach ( $errors as $msg ) { echo " - $msg<br>" ; }
+    echo 'Please try again or <a href="register.php">Register</a></p>' ;
+}?>
 
             </div>
           </div>
