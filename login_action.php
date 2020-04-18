a61920f54bfe095a08323770fed0e483d3a3203d
KeronGoodridgeMac <KeronGoodridgeMac@kerons-mbp.broadband>
2020-04-18 13:43:17 +0100

    Redirect changed to the shop as opposed to the home page. The main reason for the change was to place focus on the shop.

--- login_action.php
+++ login_action.php
@@ -20,7 +20,7 @@ if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' )
     $_SESSION[ 'user_id' ] = $data[ 'user_id' ] ;
     $_SESSION[ 'first_name' ] = $data[ 'first_name' ] ;
     $_SESSION[ 'last_name' ] = $data[ 'last_name' ] ;
-    load ( 'home.php' ) ;
+    load ( 'shop.php' ) ;
   }
   # Or on failure set errors.
   else { $errors = $data; } 
