3243f519387134187f5e7920dff094665388e814
KeronGoodridgeMac <KeronGoodridgeMac@kerons-mbp.broadband>
2020-04-22 21:51:41 +0100

    Cart css added and navigation updated as 'forum.php' page. Cart is finalised as per 'cart3.php'.

--- cart.php
+++ cart.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="includes/cart.css">
+
 <?php # DISPLAY SHOPPING CART PAGE.
 
 # Access session.
@@ -11,9 +13,17 @@ $page_title = 'Cart' ;
 include ( 'includes/header.html' ) ;
 include ('includes/menu.html' ) ;
 
-# Create navigation links.
-echo '<p><a href="checkout.php?total='.$total.'">Checkout</a> | <a href="goodbye.php">Logout</a></p>' ;
 
+# Create navigation links.
+if ($_SESSION['Firstname'] = "") {
+    # Display body section.if logged in
+    echo "<div class='secnav'><h1></h1><p>You are not logged in </p></div>";
+    echo '<div class="secnav"><p> <a href="goodbye.php">Logout</a></p></div>' ;}
+    else {
+        echo "<div class='secnav'><h1></h1><p>You are logged in as:<br> {$_SESSION['first_name']} {$_SESSION['last_name']} </p>";
+        echo '<p><a href="goodbye.php">Logout</a></p>';
+        echo '</div>';
+    }  
 
 # Check if form has been submitted for update.
 if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
@@ -47,69 +57,62 @@ if (!empty($_SESSION['cart']))
   $r = mysqli_query ($dbc, $q);
 
   # Display body section with a form and a table.
-  echo '<form action="cart.php" method="post">
-<table>
-<tr>
-<th colspan="5">Items in your cart</th></tr>
-<tr>';
+  echo '<form action="cart.php" method="post">';
   while ($row = mysqli_fetch_array ($r, MYSQLI_ASSOC))
   {
     # Calculate sub-totals and grand total.
     $subtotal = $_SESSION['cart'][$row['item_id']]['quantity'] * $_SESSION['cart'][$row['item_id']]['price'];
     $total += $subtotal;
-    
-    
 
     # Display the row/s:
+    echo'<div class="container-fluid mt-100">
+      <div class="row">
+      <div class="col-md-12">
+      <div class="card mb-4">
+      <div class="card-header">
+      <div class="media flex-wrap w-100 align-items-center"> <img class ="thumbnail" src="'.$row['item_img'].'" class="d-block ui-w-40 rounded-circle" alt="">
+      <div class="media-body ml-3"> <data-abc="true"><H4>'.$row['item_name'].' </H4><br><H5> £'. $row['item_price'] . '</H5>
+      </div>
+      <div class="text-muted small ml-3">
+      <div>Sub Total:<H4><strong>£'.number_format ($subtotal, 2).'</H5></strong></div>';
+    echo'
+      </div>
+      </div>
+      <div class="card-body">';
+  echo'</div>
+      <div class="card-footer d-flex flex-wrap justify-content-between align-items-center px-0 pt-0 pb-3">
+      <div class="px-4 pt-3"> <class="text-muted d-inline-flex align-items-center align-middle" data-abc="true"> <i class="fa fa-heart text-danger"></i>&nbsp; <span class="align-middle">Quantity:</span> </a> <span class="text-muted d-inline-flex align-items-center align-middle ml-4">'; 
 
-    
-    echo '<ul class="cartWrap">
-    <li class="items odd">
-    
-    <div class="infoWrap">
-    <div class="cartSection">
-    
-    <img class="thumbnail" src="'.$row['item_img'].'" alt="" class="itemImg" />
-    <h3>'.$row['item_name'].'</h3>
-    <h4>'.$row['item_desc'].'</h4>
-    <p> <input type="text"  class="qty" name="qty['.$row['item_id'].']" value='.$_SESSION['cart'].''.$row['item_id'].''.$row['quantity'].'/> x '.$row['item_price'].'</p>
-    <div class="$total">
-    <p>Sub-Total: £'.number_format ($subtotal, 2).'</p>
-    </div>
-   
-    </div>
-    </div>
-    </li>';
-    
-    echo 
-    
-    "<tr> 
+echo "<span><input type=\"text\" size=\"3\" name=\"qty[{$row['item_id']}]\" value=\"{$_SESSION['cart'][$row['item_id']]['quantity']}\"></span>
+    </span>";
 
-    <input type=\"text\" size=\"3\" name=\"qty[{$row['item_id']}]\" value=\"{$_SESSION['cart'][$row['item_id']]['quantity']}\"></tr>
-    <td>@ {$row['item_price']} = </td> <td>".number_format ($subtotal, 2)."</td></tr>";
-    
-
+  echo'<i class="fa fa-eye text-muted fsize-3"></i>&nbsp; <span class="align-middle"></span> </span> </div>
+        </div>
+      </div>
+      </div>
+      </div>';
   }
   
   # Close the database connection.
   mysqli_close($dbc); 
   
-  # Display the total.
-  echo 
-  
-  ' <tr><td colspan="5" style="text-align:right">Total = '.number_format($total,2).'</td></tr>
-</table>
+  # Display the cart buttons.
+  echo ' 
+     <div class="col-md-12 col-lg-4"> 
+<button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Update my cart</button><br>
 
-<input type="submit" name="submit" value="Update My Cart">';
+<a href="checkout.php?total='.$total.'"><button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Checkout</button></a></br>
+</div>';
   
+  # Display the sub-totals.
   echo'
     <div class="subtotal cf">
-    <ul>
-    <li class="totalRow"><span class="label">Standard Shipping</span><span class="value">: £5.00</span></li>
-    <li class="totalRow final"><span class="label">Total</span><span class="value">: £'.number_format($total,2).'</span></li>
-    <li class="totalRow"><a href="checkout.php?total='.$total.'" class="btn continue">Checkout</a></li>
-    </ul>
-    </div></div>';
+    <H6>
+    <div class="totalRow"><span class="label">Standard Shipping</span><span class="value">: £5.00</span></div></H6>
+    <H3><div class="totalRow final"><span class="label">Total</span><span class="value">: £'.number_format($total,2).'</span></div>
+    </H3>
+</div>
+</div>';
 }
 else
 # Or display a message.
