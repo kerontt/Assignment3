13a68d4e4c07b2088c419960fc941fa04a0c3b74
KeronGoodridgeMac <KeronGoodridgeMac@kerons-mbp.broadband>
2020-04-22 10:44:05 +0100

    Added 'if' condition to display user name if logged into the shop page and display links accordingly. i.e if loggedd in display name and 'logout' option. Alternatively, if not logged in display 'login'

--- shop.php
+++ shop.php
@@ -10,8 +10,25 @@ include ( 'includes/header.html' ) ;
 include ( 'includes/menu.html' ) ;
 
 
+
 # Create navigation links.
-echo '<div class="container-fluid"><p> <a href="goodbye.php">Logout</a></p></div>' ;
+
+
+if ($_SESSION['Firstname'] = "") {
+    # Display body section.if logged in
+    echo "<h1></h1><p>You are not logged in </p>";
+    
+    
+    
+    # Create navigation links.
+    echo '<div class="container-fluid"><p> <a href="goodbye.php">Logout</a></p></div>' ;}
+else {
+    
+    echo "<h1></h1><p>You are logged in {$_SESSION['first_name']} {$_SESSION['last_name']} </p>";
+    echo '<p><a href="goodbye.php">Logout</a></p>';
+    
+}
+
 
 # Display sort criteria.
 echo '<div class="container">';
