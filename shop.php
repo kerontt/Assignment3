48b5fe65803ea57674ef0fadb8b334e9a007cc08
KeronGoodridgeMac <KeronGoodridgeMac@kerons-mbp.broadband>
2020-04-22 19:01:13 +0100

    'shop.php' now mirrors 'shop2.php as the concept is near finalisation.

--- shop.php
+++ shop.php
@@ -12,75 +12,102 @@ include ( 'includes/menu.html' ) ;
 
 
 # Create navigation links.
-
-
 if ($_SESSION['Firstname'] = "") {
     # Display body section.if logged in
-    echo "<h1></h1><p>You are not logged in </p>";
+    echo "<div class='secnav'><h1></h1><p>You are not logged in </p></div>";
+    echo '<div class="secnav"><p> <a href="goodbye.php">Logout</a></p></div>' ;}
+    else {
+        echo "<div class='secnav'><h1></h1><p>You are logged in as:<br> {$_SESSION['first_name']} {$_SESSION['last_name']} </p>";
+        echo '<p><a href="goodbye.php">Logout</a></p>';
+        echo '</div>';
+    }
     
     
     
-    # Create navigation links.
-    echo '<div class="container-fluid"><p> <a href="goodbye.php">Logout</a></p></div>' ;}
-else {
     
-    echo "<h1></h1><p>You are logged in as:<br> {$_SESSION['first_name']} {$_SESSION['last_name']} </p>";
-    echo '<p><a href="goodbye.php">Logout</a></p>';
+    // index end
+    # Open database connection.
+    require ( 'connect_db.php' ) ;
     
-}
-
-
-# Display sort criteria.
-echo '<div class="container">';
-echo '<a href="shop_parsing.php?orderby=item_name&direction=ASC">Sort by Name (ascending)<br></a>';
-echo '<a href="shop_parsing.php?orderby=item_price&direction=ASC">Sort by Price (ascending)<br></a>';
-
- 
-
-// index end
-# Open database connection.
-require ( 'connect_db.php' ) ;
-
-# Retrieve items from 'shop' database table.
-$q = "SELECT * FROM shop" ;
-$r = mysqli_query( $dbc, $q ) ;
-if ( mysqli_num_rows( $r ) > 0 )
-{
+    //Default sorting of items.
+    $order="item_name";
+    $sort="ASC";
+    $property="item_price";
+    $minimum="200";
     
-    $thisItem = "item_name";
-    $thisItemDesc = "item_desc";
-    $thisItemImage = "item_img";
-    $thisItemPrice = "item_price";
-    $thisItemID = "item_id";
-  # Display body section.
-  echo '<div class="container">';
-
-  while ( $row = mysqli_fetch_array( $r, MYSQLI_ASSOC ))
-  {
-  echo '<a href="detail.php?id='.$row[$thisItemID].'">';
-  echo '<div class="product"><strong>' . $row[$thisItem] .'</strong><br><span style="font-size:smaller">'. $row[$thisItemDesc] . '</span><br>';
-echo '<img class="thumbnail" src='. $row[$thisItemImage].'></a>';
-echo'<br>£' . $row[$thisItemPrice] . '<br>';
-echo '<br><a href="added.php?id='.$row[$thisItemID].'" class="btn btn-primary" role="button">Add To Cart</a><br>
+    # Get passed product order value.
+    if ( isset( $_GET['order'] ) ) $order = $_GET['order'] ;
+    
+    
+    # Get passed product sort direction.
+    if ( isset( $_GET['sort'] ) ) $sort = $_GET['sort'] ;
+    
+    # Get passed product item value.
+    if ( isset( $_GET['property'] ) ) $property = $_GET['property'] ;
+    
+    
+    # Get passed product amount.
+    if ( isset( $_GET['minimum'] ) ) $minimum = $_GET['minimum'] ;
+    
+    
+    
+    
+    # Retrieve items from 'shop' database table.
+    $q = "SELECT * FROM shop ORDER by $order $sort";
+    $r = mysqli_query( $dbc, $q ) ;
+    
+    if ( mysqli_num_rows( $r ) > 0 )
+    {
+        
+        $thisItem = "item_name";
+        $thisItemDesc = "item_desc";
+        $thisItemImage = "item_img";
+        $thisItemPrice = "item_price";
+        $thisItemID = "item_id";
+        # Display body section.
+        
+        
+        
+        echo '<div class="container">';
+        
+        # Display sort criteria.
+        echo '<a href="?order=item_name&sort=ASC">Sort by Name (ascending) | </a>';
+        echo '<a href="?order=item_name&sort=DESC">Sort by Name (descending) | </a>';
+        echo '<a href="?order=item_price&sort=ASC">Sort by Price ascending) | </a>';
+        echo '<a href="?order=item_price&sort=DESC">Sort by Price (decending)<br></a>';
+        
+        # Display filter query
+        $q = "SELECT * FROM shop WHERE item_price < 200";
+        echo '<a href="?property=item_price&minimum=200">200 value<br></a>';
+        
+        
+        
+        while ( $row = mysqli_fetch_array( $r, MYSQLI_ASSOC ))
+        {
+            echo '<a href="detail.php?id='.$row[$thisItemID].'">';
+            echo '<div class="product"><strong>' . $row[$thisItem] .'</strong><br><span style="font-size:smaller">'. $row[$thisItemDesc] . '</span><br>';
+            echo '<img class="thumbnail" src='. $row[$thisItemImage].'></a>';
+            echo'<br>£' . $row[$thisItemPrice] . '<br>';
+            echo '<br><a href="added.php?id='.$row[$thisItemID].'" class="btn btn-primary" role="button">Add To Cart</a><br>
 </div>';
+            
+            
+            
+            
+        }
+        ;
+        echo '</div>';
+        echo '</div>';
+        
+        # Close database connection.
+        mysqli_close( $dbc ) ;
+    }
+    # Or display message.
+    else { echo '<p>There are currently no items in this shop.</p>' ; }
     
-
-
+    # Display footer section.
     
-  }
-  ;
-  echo '</div>';
-  echo '</div>';
-  
-  # Close database connection.
-  mysqli_close( $dbc ) ; 
-}
-# Or display message.
-else { echo '<p>There are currently no items in this shop.</p>' ; }
-
-# Display footer section.
-
-
-
-?>
+    
+    
+    ?>
 
--- shop2.php
+++ shop2.php
@@ -15,14 +15,12 @@ include ( 'includes/menu.html' ) ;
 if ($_SESSION['Firstname'] = "") {
     # Display body section.if logged in
     echo "<div class='secnav'><h1></h1><p>You are not logged in </p></div>";
-    
     echo '<div class="secnav"><p> <a href="goodbye.php">Logout</a></p></div>' ;}
     else {
         echo "<div class='secnav'><h1></h1><p>You are logged in as:<br> {$_SESSION['first_name']} {$_SESSION['last_name']} </p>";
         echo '<p><a href="goodbye.php">Logout</a></p>';
         echo '</div>';
-        
-    }
+    }   
     
     
  
