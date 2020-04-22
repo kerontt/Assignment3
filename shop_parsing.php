60e7cb4f3caf21d83f69adc3177c3ef3a64030b7
KeronGoodridgeMac <KeronGoodridgeMac@kerons-mbp.broadband>
2020-04-22 13:31:08 +0100

    Variables changed to understand the parsing process and make the sort function work. Quite remarkably, I struggled to understand this concept. However, it is now understood after wating a few YouTube videos.

--- shop_parsing.php
+++ shop_parsing.php
@@ -1,32 +1,17 @@
 <?php
 
-# Set page title and display header section.
-$page_title = 'Cart Addition' ;
-include ( 'includes/header.html' ) ;
-include ( 'includes/menu.html' ) ;
-
-
-# Get passed product id and assign it to a variable.
-if ( isset( $_GET['id'] ) ) $id = $_GET['id'] ;
-
-# Open database connection.
-require ( 'connect_db.php' ) ;
-
 #Variables; get order value and direction;
-$order_value="";
-$order_dir="";
+$order="";
+$sort="";
 
 # Get passed product order value.
-if ( isset( $_GET['order_value'] ) ) $order_value = $_GET['order_value'] ; 
+if ( isset( $_GET['order'] ) ) $order = $_GET['order'] ; 
 
 # Get passed product iorder direction.
-if ( isset( $_GET['order_dir'] ) ) $order_dir = $_GET['order_dir'] ; 
+if ( isset( $_GET['sort'] ) ) $sort = $_GET['sort'] ; 
 
-echo $order_value;
-echo $order_dir;
-
-echo SELECT * from shop order by $order_value $order_dir;
+$result= "SELECT * from shop ORDER by $order $sort";
 
 ?>
 
-<a href="shop_parsing.php?orderby=item_price&direction=ASC">Oder by price Cheapest First</a>"
+
